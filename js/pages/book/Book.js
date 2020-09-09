6f462f24870b1934780f6c2f225767b0172956fe
Lilian Mahut <lilian.mahut@pm.me>
2020-09-09 18:23:41 +0200

    ok

--- js/pages/book/Book.js
+++ js/pages/book/Book.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import api from "../../utils/api"
+import Button from "../../components/divers/Button"
 
-export default ({ match }) => {
+export default ({ match, nextBorrow }) => {
 	let bookSlug = match.params.bookSlug
 	let [book, setBook] = useState(null)
 
@@ -22,18 +23,37 @@ export default ({ match }) => {
 							<b>{book.title}</b>
 						</h1>
 						<h5>
-							<b>{book.publication ? book.publication.substring(0, 4) : "/"}</b>
+							<b>
+								Published in{" "}
+								{book.publication ? book.publication.substring(0, 4) : "/"}
+							</b>
 						</h5>
-						<h3>
-							<b>Resume</b>
-						</h3>
-						<p></p>
-						image
-						<h3>
-							<b>Author information</b>
-						</h3>
-						<p>{book.author}</p>
-						<p></p>
+						<div>
+							<h3>
+								<b>Resume</b>
+							</h3>
+							<p></p>
+						</div>
+						<div>
+							<h3>
+								<b>Author information</b>
+							</h3>
+							<i>
+								{book.authors.length > 0 &&
+									book.authors
+										.map(author => author.name)
+										.reduce((acc, cur) => `${acc}, ${cur}`)}
+							</i>
+						</div>
+						{nextBorrow && (
+							<div className='borrowButton'>
+								<Button
+									text='Borrow'
+									small={true}
+									onClick={() => returnBook(book.id)}
+								/>
+							</div>
+						)}
 					</div>
 					<div className='right'>
 						<div
